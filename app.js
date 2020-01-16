5350dafd740c072e4b93350abd3b06a69e6097b9
Emanuel Catriquil <56092756+ecatriquil@users.noreply.github.com>
2020-01-16 09:24:47 -0300

    Yelpcamp v11

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express'),
     bodyParser = require('body-parser'),
     mongoose = require('mongoose'),
+    flash = require('connect-flash'),
     passport = require('passport'),
     LocalStrategy = require('passport-local'),
     methodOverride = require('method-override'),
@@ -20,6 +21,7 @@ mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
 
 app.set('view engine', 'ejs');
 app.use(methodOverride('_method'));
+app.use(flash());
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(require('express-session')({
@@ -36,6 +38,8 @@ passport.deserializeUser(User.deserializeUser());
 
 app.use(function(req, res , next){
     res.locals.currentUser = req.user;
+    res.locals.error = req.flash('error');
+    res.locals.success = req.flash('success');
     next();
 });
 
--- routes/index.js
+++ routes/index.js
@@ -16,17 +16,18 @@ router.post('/register', function(req, res){
     const newUser = new User({username: req.body.username});
     User.register(newUser, req.body.password, function(err, user){
         if (err) {
-            console.log(err);
-            return res.render('register');
+            req.flash('error', err.message);
+            res.render('register');
         }
         passport.authenticate('local')(req,res,function(){
+            req.flash('success', `Welcome to YelpCamp ${req.body.username}`);
             res.redirect('/campgrounds');
         });
     });
 });
 
 router.get('/login', function(req,res){
-    res.render('login');
+    res.render('login', {message: req.flash('error')});
 });
 
 router.post('/login', passport.authenticate('local',
@@ -37,6 +38,7 @@ router.post('/login', passport.authenticate('local',
 
 router.get('/logout', function(req,res){
     req.logOut();
+    req.flash('success', 'Logged you out');
     res.redirect('/campgrounds');
 });
 
